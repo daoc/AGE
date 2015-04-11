a8f8858f4b037605fa22021322a9f1b107bf39ea
daoc <diego.ordonez@gmail.com>
2015-04-11 16:32:32 -0500

    Campos y métodos ordenados por categorías

--- src/daoc/age/Poblacion.java
+++ src/daoc/age/Poblacion.java
@@ -11,28 +11,33 @@ import java.util.Random;
  * @author dordonez@ute.edu.ec
  */
 public class Poblacion {
+    // General
     public final Random random;
-    private Reporte reporte;
+    private List<Individuo> poblacion;
+    private final Long tiempoInicio;
+    private int numGeneraciones;
+    // Parámetros
     private Integer numIndividuos;
     private Integer numAtributos;
-    private List<Individuo> poblacion;
-    private Generacion generacion;
-    private FuncionAptitud f;
-    private Seleccion seleccion;
-    private Cruce cruce;
-    private Integer metaAptitud;
-    private Integer maxNumGeneraciones;
-    private final Long tiempoInicio;
-    private Long maxTiempoCalculo;
-    private int numGeneraciones;
     private double probMutacion;
     private double tasaElitismo;
+    private FuncionAptitud f;    
+    private Generacion generacion;
+    private Seleccion seleccion;
+    private Cruce cruce;
+    private Reporte reporte;
+    // Finalización
+    private Integer metaAptitud;
+    private Integer maxNumGeneraciones;
+    private Long maxTiempoCalculo;
     
     public Poblacion() {
         random = new Random();
-        setReporteador(new Reporte());
+        setReporteador(new Reporte());//Reporteador por defecto. Puede reemplazarlo por otro.
         tiempoInicio = System.currentTimeMillis();
     }
+    
+    //Procesamiento
     
     public Poblacion evolucionar() {
         poblacion = new ArrayList<>(numIndividuos);
@@ -74,6 +79,29 @@ public class Poblacion {
         return false;
     }
     
+    private void generarPoblacion() {
+        poblacion = generacion.generarPoblacionInicial(this);
+    } 
+    
+    private void evaluarPoblacion() {
+        for(Individuo i : poblacion) {
+            i.setAptitud(f.aptitudIndividuo(i));
+        }
+        Collections.sort(poblacion);
+        if(f.isOrdenNatural()) {
+            Collections.reverse(poblacion);
+        }
+    }    
+    
+    private void seleccionarPoblacion() {    
+        poblacion = seleccion.nuevaPoblacion(this);
+    }
+    
+    private void cruzarIndividuos() {
+        Collections.shuffle(poblacion);
+        poblacion = cruce.cruzarPoblacion(this);
+    }
+    
     private void mutarIndividuos() {
         for(int i = 0; i < numIndividuos; i++) {
             for(int j = 0; j < numAtributos; j++) {
@@ -84,90 +112,56 @@ public class Poblacion {
             }
         }
     }
-    
-    private void cruzarIndividuos() {
-        Collections.shuffle(poblacion);
-        poblacion = cruce.cruzarPoblacion(this);
-    }
-    
-    private void seleccionarPoblacion() {    
-        poblacion = seleccion.nuevaPoblacion(this);
-    }
-    
-    private void evaluarPoblacion() {
-        for(Individuo i : poblacion) {
-            i.setAptitud(f.aptitudIndividuo(i));
-        }
-        Collections.sort(poblacion);
-        if(f.isOrdenNatural()) {
-            Collections.reverse(poblacion);
-        }
-    }
-    
-    private void generarPoblacion() {
-        poblacion = generacion.generarPoblacionInicial(this);
-    } 
-    
-    public List<Individuo> getPoblacion() {
-        return poblacion;
-    }
-    
-    public Individuo getIndividuo(int indice) {
-        return poblacion.get(indice);
-    }
 
-    public Integer getNumIndividuos() {
-        return numIndividuos;
-    }
-
-    public Integer getNumAtributos() {
-        return numAtributos;
-    }
-    
-    public int getNumGeneraciones() {
-        return numGeneraciones;
-    }
-
-    public double getProbMutacion() {
-        return probMutacion;
-    }
-
-    public double getTasaElitismo() {
-        return tasaElitismo;
-    }
-   
-    public Individuo getMasApto() {
-        return getIndividuo(0);
-    }
-    
-    public long getTiempoCalculo() {
-        return System.currentTimeMillis() - tiempoInicio;
-    }
-    
-    public final Poblacion setReporteador(Reporte reporte) {
-        reporte.setPoblacion(this);
-        this.reporte = reporte;
-        return this;
-    }
-    
-    public Poblacion setTasaElitismo(double tasaElitismo) {
-        this.tasaElitismo = tasaElitismo;
-        return this;
-    }
+    // Parametrización del sistema
     
     public Poblacion setNumIndividuos(int numIndividuos) {
         this.numIndividuos = numIndividuos;
         return this;
+    }        
+    
+    public Poblacion setNumAtributos(int numAtributos) {
+        this.numAtributos = numAtributos;
+        return this;
+    }    
+        
+    public Poblacion setProbabilidadMutacion(double probMutacion) {
+        this.probMutacion = probMutacion;
+        return this;
     }    
     
+    public Poblacion setTasaElitismo(double tasaElitismo) {
+        this.tasaElitismo = tasaElitismo;
+        return this;
+    }    
+            
     public Poblacion setFuncionAptitud(FuncionAptitud f) {
         this.f = f;
         return this;
     }
-    
-    public FuncionAptitud getFuncionAptitud() {
-        return f;
+             
+    public Poblacion setMetodoGeneracion(Generacion generacion) {
+        this.generacion = generacion;
+        return this;
     }    
+        
+    public Poblacion setMetodoSeleccion(Seleccion seleccion) {
+        this.seleccion = seleccion;
+        return this;
+    }    
+        
+    public Poblacion setMetodoCruce(Cruce cruce) {
+        this.cruce = cruce;
+        return this;
+    }     
+        
+    public final Poblacion setReporteador(Reporte reporte) {
+        reporte.setPoblacion(this);
+        this.reporte = reporte;
+        return this;
+    }    
+    
+    // Condiciones de finalización
     
     public Poblacion setMetaAptitud(int metaAptitud) {
         this.metaAptitud = metaAptitud;
@@ -187,41 +181,60 @@ public class Poblacion {
         return this;
     }     
     
-    public Poblacion setNumAtributos(int numAtributos) {
-        this.numAtributos = numAtributos;
-        return this;
+    // Obtener información de los parámetros
+    
+    public Integer getNumIndividuos() {
+        return numIndividuos;
+    }
+
+    public Integer getNumAtributos() {
+        return numAtributos;
     }
     
-    public Poblacion setProbabilidadMutacion(double probMutacion) {
-        this.probMutacion = probMutacion;
-        return this;
+    public double getProbMutacion() {
+        return probMutacion;
     }
-    
-    public Poblacion setMetodoGeneracion(Generacion generacion) {
-        this.generacion = generacion;
-        return this;
+
+    public double getTasaElitismo() {
+        return tasaElitismo;
+    }
+
+    public FuncionAptitud getFuncionAptitud() {
+        return f;
     }    
-    
+
     public Generacion getMetodoGeneracion() {
         return generacion;
     }
-    
-    public Poblacion setMetodoSeleccion(Seleccion seleccion) {
-        this.seleccion = seleccion;
-        return this;
-    }    
-    
+
     public Seleccion getMetodoSeleccion() {
         return seleccion;
     }    
-    
-    public Poblacion setMetodoCruce(Cruce cruce) {
-        this.cruce = cruce;
-        return this;
-    }     
-    
+
     public Cruce getMetodoCruce() {
         return cruce;
-    }       
+    }           
     
+    //Obtener información del entorno o del estado del trabajo
+        
+    public List<Individuo> getPoblacion() {
+        return poblacion;
+    }
+    
+    public Individuo getIndividuo(int indice) {
+        return poblacion.get(indice);
+    }
+       
+    public Individuo getMasApto() {
+        return getIndividuo(0);
+    }
+    
+    public int getNumGeneraciones() {
+        return numGeneraciones;
+    }    
+    
+    public long getTiempoCalculo() {
+        return System.currentTimeMillis() - tiempoInicio;
+    }
+         
 }
