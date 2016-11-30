cf159dfe654f1ac025f0089925bc6cb283af8a18
dordonezute <dordonez@ute.edu.ec>
2016-11-30 09:17:11 -0500

    Sudoku en curso

--- src/daoc/age/ejemplos/FiltroSudoku.java
+++ src/daoc/age/ejemplos/FiltroSudoku.java
@@ -29,63 +29,71 @@ public class FiltroSudoku implements Filtro {
     }    
     
     @Override
+    /*Filtra filas para que queden valores únicos*/
     public List<Individuo> filtrarAtributosPoblacion(Poblacion poblacion) {
-        //Filtra filas para que queden valores únicos
         //Solo para facilitar la manipulación de los individuos
         List<Individuo> individuos = poblacion.getPoblacion();
-        Set<Integer> indicesBase = IntStream.range(0, 10).boxed().collect(Collectors.toSet());
-        //Crea arreglo con los valores únicos entre [min - max]
-        Set<Integer> valoresBase = new HashSet<>();
-        for(int i = 1; i <= 9; i++) {
-            valoresBase.add(i);
-        }
+        Set<Integer> indicesBase = IntStream.range(0, 81).boxed().collect(Collectors.toSet());
+        //Crea arreglo con los valores únicos entre [min - max[
+        Set<Integer> valoresBase = IntStream.range(1, 10).boxed().collect(Collectors.toSet());
         Set<Integer> valoresRestantes = new HashSet<>();
         Set<Integer> valoresEncontrados = new HashSet<>();
+        Set<Integer> indicesRestringidos = new HashSet<>();
+        Set<Integer> indicesLibres = new HashSet<>();
         Queue<Integer> indicesDuplicados = new LinkedList<>();
         for(Individuo ind : individuos) {
             for (int f = 0; f < 9; f++) {
                 valoresEncontrados.clear();
                 valoresRestantes.addAll(valoresBase);
                 indicesDuplicados.clear();
+                indicesLibres.clear();
                 //añadir índices y valores en la restricción en primer lugar
                 //dado que estos NO pueden cambiar
-                int numRestricciones = addRestricciones(valoresRestantes, valoresEncontrados);
-                for (int c = 0; c < 9; c++) {
-                    Integer val = getValor(f, c, ind.getAtributos(), 9);
-                    if (valoresEncontrados.contains(val)) {
-                        indicesDuplicados.add(f*9+c);
-                    } else {
-                        valoresEncontrados.add(val);
-                        valoresRestantes.remove(val);
-                    }
-                }
+                addRestricciones(f, indicesRestringidos, indicesLibres, valoresRestantes, valoresEncontrados);
+                Iterator<Integer> idx_libres = indicesLibres.iterator();
                 Iterator<Integer> vals = valoresRestantes.iterator();
-                Iterator<Integer> idxs = indicesDuplicados.iterator();
-                while(idxs.hasNext()) {
-                    ind.getAtributos()[idxs.next()] = vals.next();
-                }                
+                while(idx_libres.hasNext()) {
+                    Integer idx = idx_libres.next();
+                    Integer val = ind.getAtributos()[idx];
+                    if (valoresEncontrados.contains(val)) {
+                        ind.getAtributos()[idx] = vals.next();
+                    }
+                    valoresEncontrados.add(val);
+                    //valoresRestantes.remove(val);
+                } 
+//                for (int c = 0; c < 9; c++) {
+//                    Integer val = getValor(f, c, ind.getAtributos(), 9);
+//                    if (valoresEncontrados.contains(val)) {
+//                        indicesDuplicados.add(f*9+c);
+//                    } else {
+//                        valoresEncontrados.add(val);
+//                        valoresRestantes.remove(val);
+//                    }
+//                }
+//                
+//                Iterator<Integer> idxs = indicesDuplicados.iterator();
+//                while(idxs.hasNext()) {
+//                    ind.getAtributos()[idxs.next()] = vals.next();
+//                }                
             }
         }  
         return individuos;          
     }
     
-    private int addRestricciones(Set<Integer> valoresRestantes, Set<Integer> valoresEncontrados) {
+    private void addRestricciones(int fila, Set<Integer> indicesRestringidos, Set<Integer> indicesLibres
+                                , Set<Integer> valoresRestantes, Set<Integer> valoresEncontrados) {
         //esto hay que cambiar; se debe pasar como argumento!!!
         Restriccion restriccion = new RestriccionSudoku();
         
-        Set val = new HashSet(9);
-        int crash = 0;
-//        for(int f = 0; f < 9; f++) {
-//            val.clear();
-//            for(int c = 0 ; c < 9; c++) {
-//                if(val.contains(getValor(f, c, atributos, 9))) {
-//                    crash++;
-//                } else {
-//                    val.add(getValor(f, c, atributos, 9));
-//                }
-//            }
-//        }
-        return crash;
+        for(int i = fila*9; i < fila*9+9; i++) {
+            if(restriccion.hasRestriccion(i)) {
+                indicesRestringidos.add(i);
+                valoresEncontrados.add(restriccion.getNuevoValorRestringido(i));
+                valoresRestantes.remove(restriccion.getNuevoValorRestringido(i));
+            } else {
+                indicesLibres.add(i);              
+            }
+        }
     }    
     
 }
